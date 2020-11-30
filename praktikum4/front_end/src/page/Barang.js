5a7037daf831af91eb55563dfb2eab6cce5a40d3
alfiansyah12 <alfiansyah_hafidz_27rpl@student.smktelkom-mlg.sch.id>
2020-11-30 15:11:42 +0700

    praktikum4

--- praktikum4/front_end/src/page/Barang.js
+++ praktikum4/front_end/src/page/Barang.js
@@ -1,238 +1,193 @@
-	import React, {Component} from 'react';  
-	import $ from 'jquery';  
-	import axios from 'axios';  
-	  
-	class Barang extends Component {  
-	  
-	  constructor() {  
-        super();  
-    	    this.state = {  
-               barang: [], 
-               kode_barang: "",
-       	      nama_barang: "",  
-       	      harga: "",  
-               harga: "",
-               stok: "", 
-               deskripsi: "",   
-               image: "", 
-       	      action: "",  
-       	      search: "",  
-               }
+import React, {Component} from 'react';  
+import $ from 'jquery';  
+import axios from 'axios';  
+import Modal from "../page/Modal";
+  
+class Barang extends Component {  
+  
+  constructor(){
+    super();
+    this.state = {
+        barang: [],
+        kode_barang: "",
+        nama_barang: "",
+        harga: "",
+        stok: "",
+        deskripsi: "",
+        image: "",
+        action: "",
+        message: ""
     }
-               
-              bind = (event) => {
-                this.setState({[event.target.name]: event.target.value});
-              }
-            
-              Add = () => {
-                // mengosongkan isi variabel nip, nama, dan alamat
-                // set action menjadi "insert"
-                this.setState({
-                  kode_barang: "",
-                  nama_barang: "",
-                  harga: "",
-                  stok: "", 
-               deskripsi: "",   
-               image: "", 
-                  action: "insert"
-                });
-              }
-            
-              Edit = (item) => {
-                /*
-                - mengisikan isi variabel nip, nama, alamat sesuai dengan data yang
-                akan diedit
-                - set action menjadi "update"
-                */
-                this.setState({
-                  kode_barang: "",
-                  nama_barang: "",
-                  harga: "",
-                  stok: "", 
-               deskripsi: "",   
-               image: "",
-                  action: "update"
-                });
-              }
-            
-              getBarang = () => {
-                let url = "http://localhost:8000/barang";
-                // mengakses api untuk mengambil data Barang
-                axios.get(url)
-                .then(response => {
-                  // mengisikan data dari respon API ke array Barang
-                  this.setState({barang: response.data.barang});
-                })
-                .catch(error => {
-                  console.log(error);
-                });
-              }
-            
-              findBarang = (event) => {
-                let url = "http://localhost:8000/barang";
-                if (event.keyCode === 13) {
-                  // menampung data keyword pencarian
-                  let form = {
-                    find: this.state.search
-                  }
-                  // mengakses api untuk mengambil data Barang
-                  // berdasarkan keyword
-                  axios.post(url, form)
-                  .then(response => {
-                    // mengisikan data dari respon API ke array Barang
-                    this.setState({barang: response.data.barang});
-                  })
-                  .catch(error => {
-                    console.log(error);
-                  });
-                }
-              }
-            
-              SaveBarang = (event) => {
-                event.preventDefault();
-                /* menampung data nip, nama dan alamat dari Form
-                ke dalam FormData() untuk dikirim  */
-                let url = "";
-                if (this.state.action === "insert") {
-                  url = "http://localhost:8000/barang/save"
-                } else {
-                  url = "http://localhost:8000/barang/update"
-                }
-                
-            
-                let form = {
-                  kode_barang: this.state.kode_barang,
-                  nama_barang: this.state.nama_barang,
-                  harga: this.state.harga,
-                  stok: this.state.stok,
-                  deskripsi: this.state.deskripsi,
-                  image: this.state.image,
-                }
-            
-                // mengirim data ke API untuk disimpan pada database
-                axios.post(url, form)
-                .then(response => {
-                  // jika proses simpan berhasil, memanggil data yang terbaru
-                  this.getBarang();
-                })
-                .catch(error => {
-                  console.log(error);
-                });
-                // menutup form modal
-                $("#modal").modal('hide');
-              }
-            
-              Drop = (kode_barang) => {
-                let url = "http://localhost:8000/barang/" + kode_barang;
-                // memanggil url API untuk menghapus data pada database
-                if (window.confirm('Apakah Anda yakin ingin menghapus data ini?')) {
-                  axios.delete(url)
-                  .then(response => {
-                    // jika proses hapus data berhasil, memanggil data yang terbaru
-                    this.getBarang();
-                  })
-                  .catch(error => {
-                    console.log(error);
-                  });
-                }
-              }
-            
-              componentDidMount = () => {
-                // method yang pertama kali dipanggil pada saat load page
-                this.getBarang();
-              }
-              
-         
-	    
-	  render(){  
-	    return (   
-            <div className="m-3 card">  
-              <div className="card-header bg-info text-white">Data Barang</div>  
-              <div className="card-body">  
-              <input type="text" className="form-control mb-2" name="search" value={this.state.search}  
-                onChange={this.bind} onKeyUp={this.findBarang} placeholder="Pencarian..." />  
-              {/* tampilan tabel Barang */}  
-                <table className="table">  
-	            <thead>  
-	              <tr>  
-      	            <th>Kode Barang</th>  
-                    <th>Nama Barang</th>  
-                    <th>Harga Barang</th> 
-                    <th>Stok</th>
-                    <th>Deskripsi</th> 
-                    <th>Image</th>
-                    <th>Option</th>  
-                  </tr>  
-                </thead>  
-               <tbody>  
-                  {this.state.barang.map((item,index) => {  
-                     return (  
-                       <tr key={index}>  
-                         <td>{item.kode_barang}</td>  
-                         <td>{item.nama_barang}</td>  
-                         <td>{item.harga}</td>  
-                         <td>{item.stok}</td>  
-                         <td>{item.deskripsi}</td>  
-                         <td>{item.image}</td>  
-                         <td>  
-                           <button className="btn btn-sm btn-info m-1" data-toggle="modal"  
-             	                      data-target="#modal" onClick={() => this.Edit(item)}>  
-              	                        Edit  
-         	                      </button>  
-        	                      <button className="btn btn-sm btn-danger m-1" onClick={() => this.Drop(item.kode_barang)}>  
-                	                        Hapus  
-                	                      </button>  
-                	                    </td>  
-                	                  </tr>  
-                	                );  
-                	              })}  
-                	            </tbody>
-                                
-                	          </table>  
-                	          <button className="btn btn-success" onClick={this.Add}  
-                	          data-toggle="modal" data-target="#modal">  
-                	            Tambah Data  
-                	          </button>  
-                	          {/* modal form barang */}  
-                	          <div className="modal fade" id="modal">  
-                	            <div className="modal-dialog">  
-                	              <div className="modal-content">  
-                	                <div className="modal-header">  
-                	                  Form barang  
-                	                </div>  
-                	                <form onSubmit={this.SaveBarang}>  
-                	                  <div className="modal-body">  
-                	                    Kode Barang  
-                	                    <input type="number" name="kode_barang" value={this.state.kode_barang} onChange={this.bind}  
-                	                    className="form-control" required />  
-                                      Nama Barang  
-                	                    <input type="text" name="nama_barang" value={this.state.nama_barang} onChange={this.bind}  
-                	                    className="form-control" required />
-                                      Harga Barang  
-                	                    <input type="number" name="harga" value={this.state.harga} onChange={this.bind}  
-                	                    className="form-control" required />
-                	                    Stok  
-                	                    <input type="number" name="stok" value={this.state.stok} onChange={this.bind}  
-                	                    className="form-control" required />  
-                	                    Deskripsi  
-                	                    <input type="text" name="deskripsi" value={this.state.deskripsi} onChange={this.bind}  
-                	                    className="form-control" required />  
-                                      Image  
-                	                    <input type="image" name="image" value={this.state.image} onChange={this.bind}  
-                	                    className="form-control" required />
-                	                  </div>  
-                	                  <div className="modal-footer">  
-                	                    <button className="btn btn-sm btn-success" type="submit">  
-                	                    Simpan  
-                	                    </button>  
-                	                  </div>  
-                	                </form>  
-                	              </div>  
-                	            </div>  
-                	          </div>  
-                	        </div>  
-                	      </div>  
-                	    );                 
-	  }  
-	}  
-	export default Barang;
+}
+
+bind = (event) => {
+    this.setState({[event.target.name] : event.target.value});
+}
+
+Add = () => {
+    $("#modal_barang").modal("show");
+    this.setState({
+        action: "insert",
+        kode_barang: "",
+        nama_barang: "",
+        harga: "",
+        stok: "",
+        deskripsi: "",
+        image: ""
+    });
+    document.getElementById('file').value = "";
+}
+
+Edit = (item) => {
+    $("#modal_barang").modal("show");
+    this.setState({
+        action: "update",
+        kode_barang: item.kode_barang,
+        nama_barang: item.nama_barang,
+        harga: item.harga,
+        stok: item.stok,
+        deskripsi: item.deskripsi
+    });
+    document.getElementById('file').value = "";
+}
+
+get_barang = () => {
+    let url = "http://localhost:8000/barang";
+    axios.get(url)
+    .then(response => {
+        this.setState({barang: response.data.data});
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+Drop = (kode_barang) => {
+    if (window.confirm("Apakah anda yakin ingin menghapus data ini?")) {
+        let url = "http://localhost:8000/barang/" + kode_barang;
+        axios.delete(url)
+        .then(response => {
+            this.setState({message:response.data.message});
+            this.get_barang();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+}
+
+componentDidMount = () => {
+    this.get_barang();
+}
+
+Save = (event) => {
+    event.preventDefault();
+    /* menampung data barang dari Form ke dalam FormData() untuk dikirim  */
+    let url = "";
+    if (this.state.action === "insert") {
+        url = "http://localhost:8000/barang/save"
+    } else {
+        url = "http://localhost:8000/barang/update"
+    }
+
+    let form = new FormData();
+    form.append("action", this.state.action);
+    form.append("kode_barang", this.state.kode_barang);
+    form.append("nama_barang", this.state.nama_barang);
+    form.append("harga", this.state.harga);
+    form.append("stok", this.state.stok);
+    form.append("deskripsi", this.state.deskripsi);
+    form.append("image", document.getElementById('file').files[0]);
+
+    //mengirim data ke api utk disimpan pd database
+    axios.post(url, form)
+    .then(response => {
+        //jika proses berhasil, memanggil data yg terbaru
+        this.get_barang();
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    //menutup form modal
+    $("#modal").modal('hide');
+}
+
+render() {
+    return (
+        <div className="container">
+            <div className="card mt-2">
+                <div className="card-header bg-warning">
+                    <div className="row">
+                        <div className="col-sm-8">
+                            <h4 className="text-white">Data Barang</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-body bg-tranparent">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Barang</th>
+                                <th>Harga</th>
+                                <th>Stok</th>
+                                <th>Deskripsi</th>
+                                <th>Image</th>
+                                <th>Option</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { this.state.barang.map((item) => {
+                                return(
+                                    <tr key={item.kode_barang}>
+                                        <td>{item.nama_barang}</td>
+                                        <td>{item.harga}</td>
+                                        <td>{item.stok}</td>
+                                        <td>{item.deskripsi}</td>
+                                        <td>
+                                            <img src={'http://localhost:8000/image/' + item.image}
+                                            className="img" alt="gambar" width="200px" height="200px" />
+                                        </td>
+                                        <td>
+                                            <button className="m-1 btn btn-sm btn-outline-warning" onClick={() => this.Edit(item)}>
+                                                <span className="fa fa-edit"></span>
+                                            </button>
+                                            <button className="m-1 btn btn-sm btn-danger" onClick={() => this.Drop(item.kode_barang)}>
+                                                <span className="fa fa-trash"></span>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+
+                    {/* tombol tambah */}
+                    <button className="m-3 btn btn-sm btn-outline-success" onClick={this.Add}>
+                        <span className="fa fa-plus"></span> Tambah Data
+                    </button>
+
+                    {/* form modal barang*/}
+                    <Modal id="modal_barang" title="Form Barang" bg-header="warning" text_header="dark">
+                        <form onSubmit={this.Save}>
+                        Nama Barang
+                        <input type="text" className="form-control" name="nama_barang" value={this.state.nama_barang} onChange={this.bind} required />
+                        Harga
+                        <input type="text" className="form-control" name="harga" value={this.state.harga} onChange={this.bind} required />
+                        Stok
+                        <input type="text" className="form-control" name="stok" value={this.state.stok} onChange={this.bind} required />
+                        Deskripsi
+                        <input type="text" className="form-control" name="deskripsi" value={this.state.deskripsi} onChange={this.bind} required />
+                        Image
+                        <input type="file" id="file" className="form-control" name="image" onChange={this.bind} required /> 
+                            <button className="btn btn-sm btn-success" type="submit">  
+                              Simpan  
+                            </button>  
+                          </form>
+                    </Modal>
+                </div>
+            </div>
+        </div> 
+    );
+}
+}  
+export default Barang;
---
+++ praktikum4/front_end/src/page/Modal.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+export default class Modal extends Component {
+  render(){
+    return(
+      <div className="modal fade" id={this.props.id}>
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className={ "modal-header bg-" + this.props.bg_header}>
+              <h4 className={ "text-" + this.props.text_header}>
+                {this.props.title}
+              </h4>
+            </div>
+            <div className="modal-body">
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
